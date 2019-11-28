85ad4033a2dc4d92b3ddb22a3fee4223706ff41b
Ferry <ferryhermawan864@gmail.com>
2019-11-28 21:44:47 +0700

    Tugas 17 JS

---
+++ tugas17.js
@@ -0,0 +1,17 @@
+function ubahhuruf(){
+  var huruf1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  var huruf2 = 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.'
+
+  console.log(huruf1.toLowerCase());
+  console.log(huruf1.toUpperCase());
+  console.log(huruf2.toLowerCase());
+  console.log(huruf2.toUpperCase());
+}
+ubahhuruf();
+
+function kodehuruf(){
+  var kalimat = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  console.log(kalimat.charCodeAt(0),kalimat.charCodeAt(1),kalimat.charCodeAt(2),kalimat.charCodeAt(3),kalimat.charCodeAt(4),kalimat.charCodeAt(5));
+}
+
+kodehuruf();
